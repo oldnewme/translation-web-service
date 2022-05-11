c3c1f617d981b6c3243cc5dbe71c1d2609274bca
Christian Antonio <christian.antonio@gmp-systems.com>
2022-05-11 20:19:01 +0200

    format code and add functionality to get translation from mock DB

--- index.js
+++ index.js
@@ -3,27 +3,42 @@ const DB = require('./database');
 
 const PORT = 8080;
 let server = express();
+
+// middleware
 server.use(express.json())
 
-server.get('/translation/:language_id/:key', (req, res) => {
+const findTranslation = (language_id, key, DB) => {
+    let translatedString = '';
+    DB.forEach(translation => {
+        if (translation.language_id === language_id && translation.key === key) {
+            translatedString = translation.translation;
+        }
+    });
+    return translatedString;
+}
 
+// routes
+server.get('/translation/:language_id/:key', (req, res) => {
+    const {language_id, key} = req.params;
+    res.json(findTranslation(language_id, key, DB));
 })
 
 server.post('/translation', (req, res) => {
 
     const {language_id, key, translation} = req.body;
     const translationToAdd = {
-        language_id:language_id,
-        key:key,
-        translation:translation
-    }
+        language_id: language_id,
+        key: key,
+        translation: translation
+    };
 
     DB.push(translationToAdd);
 
     res.json(translationToAdd);
     console.log(DB);
-})
+});
 
+// start server
 server.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
 });
