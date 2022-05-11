81bbe3b58eaf6eaa06eb91bd70c3b8c018e0ba86
Christian Antonio <christian.antonio@gmp-systems.com>
2022-05-11 19:47:54 +0200

    set up basic routes

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const PORT = 8080;
 let server = express();
+server.use(express.json())
+
+server.get('/translation/:language_id/:key', (req, res) => {
+
+})
+
+server.post('/translation', (req, res) => {
+
+})
 
 server.listen(PORT, () => {
 console.log(`Server is listening on port ${PORT}`)
