43140d15ba433ce6c60ab4ad95afb5987d3e91fb
Christian Antonio <christian.antonio@gmp-systems.com>
2022-05-11 19:49:30 +0200

    implement post route with mock database

---
+++ database.js
@@ -0,0 +1,19 @@
+const DB = [
+    {
+        language_id:"en",
+        key:"buy-chips",
+        translation:"Buy chips"
+    },
+    {
+        language_id:"fr",
+        key:"buy-chips",
+        translation:"Acheter des jetons"
+    },
+    {
+        language_id:"it",
+        key:"buy-chips",
+        translation:"Comprare patatine"
+    }
+]
+
+module.exports = DB;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const DB = require('./database');
 
 const PORT = 8080;
 let server = express();
@@ -10,12 +11,19 @@ server.get('/translation/:language_id/:key', (req, res) => {
 
 server.post('/translation', (req, res) => {
 
+    const {language_id, key, translation} = req.body;
+    const translationToAdd = {
+        language_id:language_id,
+        key:key,
+        translation:translation
+    }
+
+    DB.push(translationToAdd);
+
+    res.json(translationToAdd);
+    console.log(DB);
 })
 
 server.listen(PORT, () => {
-console.log(`Server is listening on port ${PORT}`)
+    console.log(`Server is listening on port ${PORT}`)
 });
-
-server.get('/' , (req, res) => {
-    res.send('Hellooooo');
-})
